96244784a309e037ba55abe063abf3e6997e57c2
m05e5 <calebmoses1111@gmail.com>
2021-01-25 16:34:08 +0100

    sessions

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,8 +6,30 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function create()
-    {
-        //
-    }
+
+        public function getSessionData(Request $request)
+        {
+            if($request->session()->has('name'))
+            {
+                echo $request->session()->get('name');
+            }
+            else
+            {
+                echo "no data in the session";
+            }
+         
+        }
+
+        public function storeSessionData(Request $request)
+        {
+            $request->session()->put('name', 'jennifer');
+            echo "data has been added to the function";
+        }
+
+        public function deleteSessionData(Request $request)
+        {
+            $request->session()->froget('name');
+            echo "Data has been removed from the session";
+        }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\FluentController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,3 +42,9 @@ Route::get('/fluent-string',[FluentController::class,'index'])->name('fluent.ind
 Route::get('/login',[LoginController::class,'index'])->name('login.index')->middleware('checkuser');
 
 Route::post('/login',[LoginController::class,'loginSubmit'])->name('login.submit');
+
+Route::get('/session/get',[SessionController::class,'getSessionData'])->name('session.get');
+
+Route::get('/session/set',[SessionController::class,'storeSessionData'])->name('session.store');
+
+Route::get('/session/remove',[SessionController::class,'deleteSessionData'])->name('session.delete');
