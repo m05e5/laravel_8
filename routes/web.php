b6b7339dfa75b908b14d2a98e9caf7360c8bfd91
m05e5 <calebmoses1111@gmail.com>
2021-01-25 14:53:04 +0100

    First commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\FluentController;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/home/{name?}', [HomeController::class, 'index'])->name('home.index'); 
+
+Route::get('/user',[UserController::class,'index'])->name('user.index');
+
+Route::get('/posts',[ClientController::class,'getAllPost'])->name('posts.getAllPost');
+
+Route::get('/posts/{id}', [ClientController::class,'getPostById'])->name('posts.getpostbyid');
+
+Route::get('/posts/1/{userid}',[ClientController::class,'getPostByUserId'])->name('posts.getpostbyuserid');
+
+Route::get('/add-post',[ClientController::class,'addPost'])->name('post.addpost');
+
+Route::get('/update-post',[ClientController::class,'updatePost'])->name('post.update');
+
+Route::get('/delete-post/{id}',[ClientController::class,'deletePost'])->name('post.delete');
+
+Route::get('/fluent-string',[FluentController::class,'index'])->name('fluent.index');
+
+Route::get('/login',[LoginController::class,'index'])->name('login.index');
+
+Route::post('/login',[LoginController::class,'loginSubmit'])->name('login.submit');
